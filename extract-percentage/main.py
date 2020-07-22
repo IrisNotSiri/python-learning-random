2abb35aeeeae6d44f8a266e65937f6b662f255e9
Iris Wu <iriswu@IrisdeMacBook-Air.local>
2020-07-22 17:51:08 -0400

    extract percentage from txt file

---
+++ extract-percentage/main.py
@@ -0,0 +1,11 @@
+with open('stdout.txt', 'r') as stdout:
+  for line in stdout:
+    splitedLine = line.split()
+    #print (line)
+    if splitedLine[1] == "Running":
+      percentage = line.split()[2]
+    elif splitedLine[1] == "Requested":
+      percentage = "0%"
+    elif splitedLine[1] == "Done":
+      percentage = "100%"
+    print (percentage)
