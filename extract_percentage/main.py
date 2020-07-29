76cd3c6636e5b65e3e494299493eaa61ce6c009d
IrisNotSiri <63980515+IrisNotSiri@users.noreply.github.com>
2020-07-29 11:15:11 -0400

    Update main.py

--- extract_percentage/main.py
+++ extract_percentage/main.py
@@ -12,19 +12,3 @@ with open('stdout.txt', 'r') as stdout:
 
 print ("Job Finished")
 
-
-list1 = [1,2,3,4]
-list2 = [list1[0],list1[1]]
-print (list2)
-"""
-import netifaces
-w = netifaces.ifaddresses("eth0")
-print ("w:"+str(w))
-"""
-import re
-interfaces = ["eth0", "eth0.220", "eth0.230", "eth0.240"]
-for interface in interfaces:
-  x=re.findall("eth0", interface)
-  print(x)
-  if x:
-    print (interface)
