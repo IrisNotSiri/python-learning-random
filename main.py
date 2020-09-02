c49b4b60991dc428f819b2856898ea28f93479d4
IrisNotSiri <63980515+IrisNotSiri@users.noreply.github.com>
2020-09-02 11:08:14 -0400

    try find if key in dictionary

--- main.py
+++ main.py
@@ -13,3 +13,19 @@ http.mount("http://", adapter)
 
 response = http.get("https://en.wikipedia.org/w/api.php")
 
+#try if key is in dictionary
+job_request = {'lanawareId': 5, 'action': 'updateAssignment', 'partnerId': 2, 'partnerName': 'WHY', 'customerId': 2, 'customerName': 'Iris Working Lab', 'siteName': 'Fancy site'}
+if 'timeZone' in job_request:
+  print (1)
+else:
+  print(2)
+
+#remove space between strings
+hostname = ""
+hostid = 2
+name = "Siri Working Lab"
+print (name.split())
+for i in name.split():
+  hostname = hostname+i
+hostname += str(hostid)
+print (hostname)
